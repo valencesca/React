cc6633d40a9fa025414fb500852c92d268f6ad57
Valentin Cesca <valen.cesca.8@gmail.com>
2022-07-07 11:29:59 +0200

    Adapto los archivos para el funcionamient o del NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, NavDropdown, Col, Nav } from 'react-bootstrap';
-import Navbar from 'react-bootstrap/Navbar';
+import NavBar from './components/NavBar';
 function App() {
-  return (
+  return(
     <div className="App">
-      <Navbar bg="dark" expand="lg" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">NIDAS</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Productos</Nav.Link>
-              <Nav.Link href="#link">Carrito</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <NavBar />
     </div>
   );
 }
